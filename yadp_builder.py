4a0f53f2bacc3e1009a09acefa7839f20872b1b4
Ben Copeland <ben.copeland@linaro.org>
2017-10-24 11:28:18 +0100

    Build the configure-yadocker-cloud.groovy.j2 template file Build the volume !include function Add dryrun and logging support

--- yadp_builder.py
+++ yadp_builder.py
@@ -5,8 +5,6 @@ from jinja2 import FileSystemLoader, Environment
 import jenkins
 import logging
 
-logging.basicConfig(level=logging.INFO)
-
 class LoaderMeta(type):
 
     def __new__(metacls, __name__, __bases__, __dict__):
@@ -54,18 +52,32 @@ def jinja2_from_template(directory, template_name, data):
 
 def get_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', '--username', type=str, required=True, help='Username for Jenkins server')
-    parser.add_argument('-p', '--password', type=str, required=True, help='Password for Jenkins server')
-    parser.add_argument('-i', '--inventory', type=str, required=True, default='hosts.yml', help='specify inventory host path')
+    parser.add_argument('-u', '--username', type=str, required=True,
+                        help='Username for Jenkins server')
+    parser.add_argument('-p', '--password', type=str, required=True,
+                        help='Password for Jenkins server')
+    parser.add_argument('-s', '--server', type=str, default='http://localhost:8080',
+                        help='Jenkins server URL')
+    parser.add_argument('-i', '--inventory', type=str, default='hosts.yml',
+                        help='specify inventory host path')
+    parser.add_argument('-l', '--loglevel', default='INFO',
+                        help="Setting logging level, default: %(default)s")
+    parser.add_argument('--dryrun', action='store_true',
+                        help='Do not publish to Jenkins')
     return parser
 
 if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
+    logging.basicConfig(level=args.loglevel)
     with open(args.inventory, 'r') as f:
         data = yaml.load(f, Loader)
+    logging.debug(data)
     template_output = jinja2_from_template('./templates', 'configure-yadocker-cloud.groovy.j2', data)
-    server = jenkins.Jenkins('http://localhost:8080', username=args.username, password=args.password)
-    info = server.run_script(template_output)
-    logging.info(info)
-
+    server = jenkins.Jenkins(args.server, username=args.username, password=args.password)
+    if args.dryrun:
+        with open('/tmp/configure-yadocker-cloud.groovy', 'w') as fw:
+            fw.write(template_output)
+    else:
+        info = server.run_script(template_output)
+        logging.info(info)
